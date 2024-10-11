c6a50d54fb2b1f2c69319ecf076bd08b3d174e4c
Ddennys <54415294+DennysRivera@users.noreply.github.com>
2024-10-11 11:20:23 -0700

    404 route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', async (req, res) => {
     res.send(`Hello World, It's me Dio!`);
 });
 
+app.get('/404', (req, res) => {
+    res.send('This page does not exist');
+}):
+
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
